fe95c28f060533831253c5e27fa1b59c5953c0d5
Jarod Wille <=>
2023-02-25 11:20:37 -0500

    Updated with first lab

--- ROS_Core/src/Labs/Lab0/scripts/controller/pure_pursuit.py
+++ ROS_Core/src/Labs/Lab0/scripts/controller/pure_pursuit.py
@@ -72,11 +72,11 @@ class PurePursuitController():
         This function sets up the publisher for the control command
         '''
         ################## TODO: 1. Set up a publisher for the ServoMsg message###################
-        # Create a publiser - self.control_pub:
+        # Create a publisher - self.control_pub:
         #   - subscribes to the topic <self.control_topic>
         #   - has message type <ServoMsg> (racecar_msgs.msg.Odometry) 
         #   - with queue size 1
-        self.control_pub = None # TO BE FILLED
+        self.control_pub = rospy.Publisher(self.control_topic, ServoMsg, queue_size=1)
         ########################### END OF TODO 1#################################
         
             
@@ -93,6 +93,7 @@ class PurePursuitController():
         #   - has message type <Odometry> (nav_msgs.msg.Odometry) 
         #   - with callback function <self.odometry_callback>, which has already been implemented
         #   - with queue size 1
+        self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odometry_callback, queue_size=1)
         ########################### END OF TODO 2#################################
         
     def odometry_callback(self, odom_msg: Odometry):
@@ -122,9 +123,10 @@ class PurePursuitController():
         # 2. Retrieve the goal from the goal message 
         #   and create a 3-dim numpy array [x,y,1]
         # 3. add the goal to the buffer (self.goal_buffer)
-        
-        goal_x = np.nan # TO BE FILLED
-        goal_y = np.nan # TO BE FILLED
+        goal_x = goal_msg.pose.position.x
+        goal_y = goal_msg.pose.position.y
+        goal_array = np.array([goal_x, goal_y, 1])
+        self.goal_buffer.writeFromNonRT(goal_array)
         
         ########################### END OF TODO 3 #################################
         # Log the goal to the console using "rospy.loginfo"
@@ -154,7 +156,11 @@ class PurePursuitController():
         # 2. Set the header time to the current time
         # 3. Set the throttle and steering angle to the servo message
         # 4. Publish the servo message
-        
+        msg = ServoMsg() #
+        msg.header.stamp = rospy.get_rostime()
+        msg.throttle, msg.steer = throttle, steer
+        self.control_pub.publish(msg)
+
         ########################### END OF TODO 4 #################################
 
     def planning_thread(self):
@@ -206,9 +212,25 @@ class PurePursuitController():
                     # 5. clip the steering angle between "-self.steer_max" and "self.steer_max"
                     # 6. apply the simple proportional controller for the acceleration to track the reference_velocity
                     
-                    accel = 0 # TO BE FILLED 
-                    steer = 0 # TO BE FILLED
+                    accel = 0
+                    steer = 0 
+                    threshold_dist = 0.4
+                    Kp = 10
+                    if dis2goal < threshold_dist:
+                        accel = -1
+                        steer = 0
+                    elif goal_robot[0] < 0:
+                        steer = self.max_steer
+                        ref_vel = self.max_vel
+                        err = (ref_vel - state_cur.v)
+                        accel = Kp*err
+                    else: 
+                        steer = np.arctan(2*self.wheel_base*np.sin(alpha)/dis2goal)
+                        steer = np.clip(steer, -self.max_steer, self.max_steer)
+                        ref_vel = self.max_vel
+                        err = (ref_vel - state_cur.v)
+                        accel = Kp*err
+
                     ########################### END OF TODO 5 ###########################################
-                    
                     # publish the control
                     self.publish_control(accel, steer, state_cur)
