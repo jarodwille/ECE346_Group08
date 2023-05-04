7dac89c0d54cd718e1d0e4da32dd42cdf7c11fec
Jarod Wille <jwille@princeton.edu>
2023-05-04 17:52:30 -0400

    task 2 simulation for just following boss ALMOST THEREgit add .!

--- ROS_Core/src/Labs/FinalProject/scripts/task2.py
+++ ROS_Core/src/Labs/FinalProject/scripts/task2.py
@@ -1,27 +1,33 @@
+#!/usr/bin/env python
 import rospy
-import np
+import numpy as np
 import yaml
 from std_srvs.srv import Empty, EmptyRequest, EmptyResponse
 from final_project.srv import Schedule, ScheduleRequest, ScheduleResponse, \
     Task, TaskRequest, TaskResponse, Reward, RewardRequest, RewardResponse
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, Path
 from racecar_routing.srv import Plan, PlanResponse, PlanRequest
 
 
 class SwiftHaulTasks:
     def __init__(self):
-
+        print("STArtED PROGRAM 0")
         self.load_warehouse_info()
         self.setup_clients()
-        self.setup_subscriber()
+        self.setup_sub_pub()
 
         # response of boss schedule
+        rospy.sleep(15)
+          # call the service to start session
         self.boss_schedule = self.boss_schedule_client(ScheduleRequest())
+        print("Starting client at", rospy.get_rostime())
+        self.start_client(EmptyRequest())
+        self.odom_msg = None
 
     def load_warehouse_info(self):
 
         # Retrieve Warehouse Information from yaml file
-        with open('../task2.yaml', 'r') as stream:
+        with open('/home/saferoboticslab/ECE346_Group08/ROS_Core/src/Labs/FinalProject/task2.yaml', 'r') as stream:
             warehouse_info = yaml.safe_load(stream)
 
         # HACK: No failsafe is implemented for invalid warehouse config
@@ -42,7 +48,6 @@ class SwiftHaulTasks:
         # set up the service client to start session
         rospy.wait_for_service('/SwiftHaul/Start')
         self.start_client = rospy.ServiceProxy('/SwiftHaul/Start', Empty)
-        self.start_client(EmptyRequest())  # call the service to start session
 
         # set up the service client to get boss schedule
         rospy.wait_for_service('/SwiftHaul/BossSchedule')
@@ -65,41 +70,69 @@ class SwiftHaulTasks:
         rospy.wait_for_service('/routing/plan')
         self.plan_client = rospy.ServiceProxy('/routing/plan', Plan)
 
-    def setup_subscriber(self):
-        self.pose_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback, queue_size=1)
+    def setup_sub_pub(self):
+        self.pose_sub = rospy.Subscriber('/Simulation/Pose', Odometry, self.odom_callback, queue_size=1)
+        self.path_pub = rospy.Publisher('/Routing/Path', Path, queue_size=10, latch = True)
 
     def odom_callback(self, odom_msg):
         self.odom_msg = odom_msg
 
-    def calculate_waypoints(self):
+    def plan_path(self):
         # If still aims for thecurrent waypoint, keep replanning.
         i = 0
         
-        while not rospy.is_shutdown() and i<len(self.goal_array):
+        while not rospy.is_shutdown() and i<self.num_warehouse:
             if self.odom_msg == None:
+                "No odom_msg received"
                 rospy.sleep(0.1)
                 continue
         
             # Start position
             x_start = self.odom_msg.pose.pose.position.x
             y_start = self.odom_msg.pose.pose.position.y
-            
-            x_goal = self.warehouse_location[self.boss_schedule_client[1]][0]# x coordinate of the goal
-            y_goal = self.warehouse_location[self.boss_schedule_client[1]][1]# y coordinate of the goal
+
+            warehouse_idx= self.boss_schedule.goal_warehouse_index[i]
+            x_goal = self.warehouse_location[warehouse_idx][0]# x coordinate of the goal
+            y_goal = self.warehouse_location[warehouse_idx][1]# y coordinate of the goal
+            dx = self.warehouse_location[warehouse_idx][2]# x width of the warehouse
+            dy = self.warehouse_location[warehouse_idx][3]# y width of the warehouse
+            print("current location", x_start, ", ", y_start)
+            print("current goal", x_goal, ", ", y_goal)
+            print("dx: ", dx, "dy: ", dy)
+            print(np.abs(x_start - x_goal))
+
             
             plan_request = PlanRequest([x_start, y_start], [x_goal, y_goal])
             plan_response = self.plan_client(plan_request)
-                
-            path_msg: Path
+            
             path_msg = plan_response.path
             path_msg.header.stamp = rospy.get_rostime()
             path_msg.header.frame_id = 'map'
             self.path_pub.publish(path_msg)
-           
 
-            dist = np.sqrt((x_start - x_goal)**2 + (y_start - y_goal)**2)   
-            if dist<0.4:
-                i +=1  
+           
+            # if we find we're within warehouse
+            if np.abs(x_start - x_goal) < dx and np.abs(y_start - y_goal) < dy:
+                print("Truck inside warehouse")
+                response = self.boss_schedule_client(RewardRequest()) # check with boss if we made it
+                if response.done == False:
+                    print("boss is unsatisfied with our arrival")
+                
+                print("Total reward so far: ", response.total_reward)
+
+                
+                # stop (plan route to self)! if boss hasn't started new task (meaning boss stopped)
+                while(self.boss_task_client(TaskRequest()).task == -1):
+                    "No new task received"
+                    plan_request = PlanRequest([x_start, y_start], [x_start, y_start])
+                    plan_response = self.plan_client(plan_request)
+                    path_msg = plan_response.path
+                    path_msg.header.stamp = rospy.get_rostime()
+                    path_msg.header.frame_id = 'map'
+                    self.path_pub.publish(path_msg)
+                    rospy.sleep(0.1)
+
+                i += 1
                 rospy.sleep(0.1)
             else:
                 rospy.sleep(1)
@@ -154,9 +187,10 @@ class SwiftHaulTasks:
 
 
 if __name__ == '__main__':
-
-    rospy.init_node('task2.py')
-
+    print("entered main")
+    rospy.init_node('ego_simulation_node')
+    print("initialized node")
     init = SwiftHaulTasks()
+    print("ran swifthaultasks")
     while not rospy.is_shutdown():
-        init.calculate_waypoints()
+        init.plan_path()
